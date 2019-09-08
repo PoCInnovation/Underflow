ac4ed5bfcdbda77987fb99812e6872a5b7d8f45f
slohan sainte-croix <slohan.sainte-croix@epitech.eu>
2019-09-08 01:19:42 +0000

    Update regular2.py

--- regular2.py
+++ regular2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-  * Copyright (c) 2019 _rollo Commit <slohan.stcroix@gmail.com>
   *
-  * 
+  * Copyright (c) 2019 _rollo Commit <slohan.stcroix@gmail.com>
   *
 """
 
